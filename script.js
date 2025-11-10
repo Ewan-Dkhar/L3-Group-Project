b4d7871d1333c3068ab632466183c8b2ec6bda6d
Ewan-Dkhar <wanedarkh@gmail.com>
2025-11-10 17:00:42 +0530

    Final integration of code from all the members.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const form = document.querySelector("form");
+const table = document.querySelector("table > tbody");
+const modalAgreeButton = document.querySelector("#agree-btn")
+const modalCloseButton = document.querySelector("#close-btn")
+const modal = new bootstrap.Modal(document.getElementById('terms'));
 
-form.addEventListener("submit", (e) => {
-    if(!form.checkValidity()){
-        e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    console.log(form.checkValidity())
+    if (form.checkValidity()) {
+      modal.show();
+    } else {
+      form.classList.add('was-validated');
     }
+});
 
-    form.classList.add("was-validated");
+modalAgreeButton.addEventListener("click", () => {
+    const name = form.floatingName.value;
+    const email = form.floatingEmail.value;
+    const number = form.floatingPhone.value;
+
+    const newRow = table.insertRow();
+    newRow.innerHTML = `<td>${name}</td>
+                <td>${email}</td>
+                <td>${number}</td>`;
+    table.appendChild(newRow);
+
+    form.reset();
+    form.classList.remove("was-validated");
 })
-
-const year = document.querySelector("#year");
-
-let d = new Date();
-year.textContent = `${d.getFullYear()}`;
+modalCloseButton.addEventListener("click", () => {
+    form.reset();
+    form.classList.remove("was-validated");
+})
